fd2ff1cc21167d4501f2fdc96a9562063ba49cd1
comettoastor <comettoastor@gmail.com>
2022-11-18 03:19:32 -0300

    koko

--- pryComettoSP3/frmMain.cs
+++ pryComettoSP3/frmMain.cs
@@ -12,16 +12,25 @@ namespace pryComettoSP3
 {
     public partial class frmMain : Form
     {
-        //DECLARACION DE VARIABLES GLOBALES (EXCLUSIVAS PARA ESTE FORMULARIO)
-        int vContadorTurnos = 0;
-        int vModeloMasAntiguo = 0;
-        int vCantidadDom6 = 0;
+        public struct structTurnos
+        {
+            public int intNumeroTurno;
+            public string strDominio;
+            public int intAnioFabricacion;
+            public string strTitular;
+        };
+        public structTurnos[] vecTurnos = new structTurnos[50];
+        public int intContador = 0;
 
-        //Vector - Array 1 Dimension
-        string[] vector = new string[0];
+        private void LimpiezaInterfaz()
+        {
+            txtTurno.Clear();
+            txtDominio.Clear();
+            nudModelo.Value = 2022;
+            txtTitular.Clear();
+            txtTurno.Focus();
+        }
 
-        //Matriz - Array 2 Dimensiones
-        string[,] vector2 = new string[0,0];
         public frmMain()
         {
             InitializeComponent();
@@ -32,37 +41,59 @@ namespace pryComettoSP3
             if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
             {
                 e.Handled = true;
-                MessageBox.Show("Solamente números","Error");
+                MessageBox.Show("Solamente números","Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void txtDominio_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if ((e.KeyChar < '0' || e.KeyChar > '9') && !char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar))
+            if (!char.IsLetterOrDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
             {
                 e.Handled = true;
+                MessageBox.Show("Solamente números o letras!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void btnRegistrar_Click(object sender, EventArgs e)
         {
-            if (txtDominio.Text.Length >= 6 && txtTitular.Text.Length >= 2)
+            bool validador = true;
+            if (txtTurno.Text != "" && txtTitular.Text != "" && txtDominio.Text != "")
             {
-                MessageBox.Show("Datos ingresados correctamente");
-                vContadorTurnos++;
-                if (nudModelo.Value < vModeloMasAntiguo || vModeloMasAntiguo == 0)
+                if (txtDominio.Text.Length >= 6 && txtTitular.Text.Length >= 2)
                 {
-                    vModeloMasAntiguo = Convert.ToInt32(nudModelo.Value);                
+                    for (int i = 0; i < intContador; i++)
+                    {
+                        if (vecTurnos[i].intNumeroTurno == int.Parse(txtTurno.Text))
+                        {
+                            validador = false;
+                        }
+                    }
+                    if (validador == true)
+                    {
+                        vecTurnos[intContador].intNumeroTurno = int.Parse(txtTurno.Text);
+                        vecTurnos[intContador].strDominio = txtDominio.Text;
+                        vecTurnos[intContador].intAnioFabricacion = Convert.ToInt32(nudModelo.Value);
+                        vecTurnos[intContador].strTitular = txtTitular.Text;
+                        intContador++;
+                        MessageBox.Show("Datos ingresados correctamente!","Éxito",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                        LimpiezaInterfaz();
+                        txtDominioSeisChars.Clear();
+                        txtModeloAntiguo.Clear();
+                        txtCantidadTurnos.Clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("El turno de registro ya ha sido registrado!","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    }
                 }
-                if (txtDominio.Text.Length == 6)
+                else
                 {
-                    vCantidadDom6++;
+                    MessageBox.Show("El número de dominio debe ser mayor o igual a 6", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-                LimpiezaInterfaz();
             }
             else
             {
-                MessageBox.Show("Faltan o hay datos ingresados incorrectamente");
+                MessageBox.Show("Faltan o hay datos ingresados incorrectamente", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -110,20 +141,28 @@ namespace pryComettoSP3
             }
         }
 
-        private void LimpiezaInterfaz()
-        {
-            txtTurno.Clear();
-            txtDominio.Clear();
-            nudModelo.Value = 2022;
-            txtTitular.Clear();
-            txtTurno.Focus();
-        }
-
         private void btnConsultar_Click(object sender, EventArgs e)
         {
-            txtCantidadTurnos.Text = vContadorTurnos.ToString();
-            txtModeloAntiguo.Text = vModeloMasAntiguo.ToString();
-            txtDominioSeisChars.Text = vCantidadDom6.ToString();
+            int contadorDominio6 = 0;
+            int modeloAntiguo = vecTurnos[0].intAnioFabricacion;
+            for (int i = 0; i < intContador; i++)
+            {
+                if (modeloAntiguo > vecTurnos[i].intAnioFabricacion)
+                {
+                    modeloAntiguo = vecTurnos[i].intAnioFabricacion;
+                }
+            }
+            txtModeloAntiguo.Text = modeloAntiguo.ToString();
+            for (int i = 0; i < intContador; i++)
+            {
+                if (vecTurnos[i].strDominio.Length == 6)
+                {
+                    contadorDominio6++;
+                }
+            }
+            txtDominioSeisChars.Text = contadorDominio6.ToString();
+            txtCantidadTurnos.Text = intContador.ToString();
+
         }
 
         private void btnSalir_Click(object sender, EventArgs e)
